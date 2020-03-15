9f8a04b6ae7b06f22a69d4165a93ad22eccc3df6
David Langmeier <dalangmeier@edu.aau.at>
2020-03-15 17:03:43 +0100

    add edittext & button, read user input

--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.se2einzelphase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // called when user taps the "Send" button
+    public void sendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        int matrNr = (Integer.parseInt(editText.getText().toString()));
+        Log.d("Matrikelnummer = ", String.valueOf(matrNr));
+        sendToServer(matrNr);
+    }
+
+    public int sendToServer(int nr) {
+        // TODO: 15.03.2020 matrNr an Server senden
+        return 0;
     }
 }
